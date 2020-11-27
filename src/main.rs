daecb45466c03df4026318f45a9a155a574210b8
Eyal Kalderon <ebkalderon@gmail.com>
2020-11-27 20:57:11 +0800

    Gracefully shut down containers on Ctrl+C

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::sync::Arc;
+
+use anyhow::anyhow;
 use dashmap::DashMap;
+use fallible_collections::FallibleArc;
 
 use self::container::Container;
 use self::image::OciImage;
@@ -9,14 +13,23 @@ mod pipe;
 
 #[derive(Debug)]
 pub struct Engine {
-    containers: DashMap<String, Container>,
+    containers: Arc<DashMap<String, Container>>,
 }
 
 impl Engine {
-    pub fn new() -> Self {
-        Engine {
-            containers: DashMap::new(),
-        }
+    pub async fn new() -> anyhow::Result<Self> {
+        let containers = Arc::try_new(DashMap::new()).map_err(|e| anyhow!("OOM error: {:?}", e))?;
+        let running = containers.clone();
+
+        // TODO: Find a way to avoid using `tokio::spawn()` and convert to `join!()` instead.
+        tokio::spawn(async move {
+            if let Ok(_) = tokio::signal::ctrl_c().await {
+                running.clear();
+                std::process::exit(130);
+            }
+        });
+
+        Ok(Engine { containers })
     }
 
     pub async fn fetch(&mut self, container_name: &str) -> anyhow::Result<()> {
@@ -34,7 +47,7 @@ impl Engine {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // TODO: Use `warp` to host REST endpoints.
-    let mut engine = Engine::new();
+    let mut engine = Engine::new().await?;
     engine.fetch("busybox").await?;
     tokio::time::sleep(std::time::Duration::from_secs(1000)).await;
     Ok(())
